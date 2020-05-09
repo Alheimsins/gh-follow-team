e4067a16b72e43b07c60ecccd311c0bd014b5c17
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-05-09 16:02:04 +0200

    Adds part one of authorize

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # gh-follow-team
-Follow all members from a team on GitHub
+
+Follow all members from a team on GitHub.
+
+## Setup
+
+You'll need to create a GitHub OAuth App.
+
+# License
+
+[MIT](LICENSE)
--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 (async () => {
+  const open = require('open')
   const setup = require('./lib/setup')
-  const { clientId, clientSecret, teamUrl} = await setup()
-  console.log(clientId, clientSecret, teamUrl)
+  const { clientID, clientSecret, teamUrl } = await setup()
+  console.log(clientID, clientSecret, teamUrl)
+
+  require('http').createServer((request, response) => {
+    if (request.url.match(/callback/)) {
+      const url = new URL(`http://${request.headers.host}${request.url}`)
+      const code = url.searchParams.get('code')
+      console.log(code)
+      response.end('You may close this window now.')
+      process.exit(0)
+    }
+  }).listen(3000)
+
+  open(`https://github.com/login/oauth/authorize?client_id=${clientID}&scope=user:follow,read:org`)
 })()
