167259a94b5f28954f720fcd2c3e60be9bbf25f1
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-05-09 22:14:16 +0200

    Removes console.log

--- index.js
+++ index.js
@@ -2,15 +2,12 @@
   const open = require('open')
   const setup = require('./lib/setup')
   const { teamUrl } = await setup()
-  console.log(teamUrl)
 
   require('http').createServer((request, response) => {
     if (request.url.match(/callback/)) {
       const url = new URL(`http://${request.headers.host}${request.url}`)
       const token = url.searchParams.get('token')
-      console.log(token)
       response.end('You may close this window now.')
-      process.exit(0)
     }
   }).listen(9000)
 
