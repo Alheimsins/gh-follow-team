bbe9dc1a2464f52b94ff76a3facefa41112d15ac
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-05-10 10:42:24 +0200

    Adds test for create-api-url

---
+++ test/create-api-url-test.js
@@ -0,0 +1,23 @@
+const test = require('ava')
+const createApiUrl = require('../lib/create-api-url')
+
+test('it returns api url from team url', t => {
+  const teamUrl = 'https://github.com/orgs/Alheimsins/teams/core/members'
+  const expectedUrl = 'https://api.github.com/orgs/Alheimsins/teams/core/members'
+  const createdUrl = createApiUrl(teamUrl)
+  t.is(createdUrl, expectedUrl, 'url is ok')
+})
+
+test('it returns api url from api url', t => {
+  const teamUrl = 'https://api.github.com/orgs/Alheimsins/teams/core/members'
+  const expectedUrl = 'https://api.github.com/orgs/Alheimsins/teams/core/members'
+  const createdUrl = createApiUrl(teamUrl)
+  t.is(createdUrl, expectedUrl, 'url is ok')
+})
+
+test('it returns api url from team url without members', t => {
+  const teamUrl = 'https://github.com/orgs/Alheimsins/teams/core'
+  const expectedUrl = 'https://api.github.com/orgs/Alheimsins/teams/core/members'
+  const createdUrl = createApiUrl(teamUrl)
+  t.is(createdUrl, expectedUrl, 'url is ok with members')
+})
